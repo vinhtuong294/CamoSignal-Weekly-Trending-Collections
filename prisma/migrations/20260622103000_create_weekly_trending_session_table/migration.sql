99f5123e7e9353abf50ccede8dca5a3914fd542c
vinhtuong294 <vinhtuong294@gmail.com>
2026-06-22 17:46:27 +0700

    Add weekly trending session migration

---
+++ prisma/migrations/20260622103000_create_weekly_trending_session_table/migration.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS "WeeklyTrendingSession" (
+    "id" TEXT NOT NULL,
+    "shop" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "isOnline" BOOLEAN NOT NULL DEFAULT false,
+    "scope" TEXT,
+    "expires" TIMESTAMP(3),
+    "accessToken" TEXT NOT NULL,
+    "userId" BIGINT,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "email" TEXT,
+    "accountOwner" BOOLEAN NOT NULL DEFAULT false,
+    "locale" TEXT,
+    "collaborator" BOOLEAN DEFAULT false,
+    "emailVerified" BOOLEAN DEFAULT false,
+    "refreshToken" TEXT,
+    "refreshTokenExpires" TIMESTAMP(3),
+
+    CONSTRAINT "WeeklyTrendingSession_pkey" PRIMARY KEY ("id")
+);
